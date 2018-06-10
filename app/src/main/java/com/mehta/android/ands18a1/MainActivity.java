3cc87c5c77f260266df5310d6129a76a939cd7a8
Mehta Sudesh <sudeshiazi@gmail.com>
2018-06-10 20:41:04 +0200

    and s18 a1

---
+++ app/src/main/java/com/mehta/android/ands18a1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.mehta.android.ands18a1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView battery;
+
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
+            battery.setText("Battery Level : "  + String.valueOf(level)+"%");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        battery = (TextView) this.findViewById(R.id.txtBattery);
+        this.registerReceiver(this.mBatInfoReceiver,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+    }
+}
